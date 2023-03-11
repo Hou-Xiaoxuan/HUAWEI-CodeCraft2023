5a3f5e3a3b4613136f8793490c0f2a8761875f6d
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2023-03-11 18:21:46 +0800

    数据读取

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,27 @@
+#include <fstream>
 #include <iostream>
 using namespace std;
+#ifndef __MAP_H__
+ #include "map.h"
+#endif
 
-bool readUntilOK() {
-    char line[1024];
-    while (fgets(line, sizeof line, stdin)) {
-        if (line[0] == 'O' && line[1] == 'K') {
-            return true;
-        }
-        //do something
+int main()
+{
+// 打开../Robot/maps/1.txt
+#ifdef DEBUG
+    ifstream fin("Robot/maps/1.txt");
+    if (!fin)
+    {
+        cerr << "open file error" << endl;
+        return 0;
     }
-    return false;
-}
-
-int main() {
-    readUntilOK();
+    init(fin);
+#else
+    init(cin);
+#endif
     puts("OK");
     fflush(stdout);
-    int frameID;
-    while (scanf("%d", &frameID) != EOF) {
-        readUntilOK();
-        printf("%d\n", frameID);
-        int lineSpeed = 3;
-        double angleSpeed = 1.5;
-        for(int robotId = 0; robotId < 4; robotId++){
-            printf("forward %d %d\n", robotId, lineSpeed);
-            printf("rotate %d %f\n", robotId, angleSpeed);
-        }
-        printf("OK\n", frameID);
-        fflush(stdout);
-    }
+    
+
     return 0;
 }
