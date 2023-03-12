c4f5f36a65c430547e460d64b8570a8537485808
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2023-03-13 01:56:52 +0800

    main_test_io

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
+#include "iointerface.h"
 #include "model.h"
-
 
 int main()
 {
-// 打开../Robot/maps/1.txt
-#ifdef DEBUG
-    ifstream fin("Robot/maps/1.txt");
-    if (!fin)
-    {
-        cerr << "open file error" << endl;
-        return 0;
-    }
-    init(fin);
-#else
     init(cin);
-#endif
     puts("OK");
     fflush(stdout);
 
+    /*----------START----------*/
+    while (cin.eof() == false)
+    {
+        cerr << "info: flame read" << endl;
+        io::read_flame(cin);
+        cerr << "info: flame read end, flame:" << meta.current_flame << endl;
 
+        vector<io::Instruction *> instructions;
+        instructions.push_back(new io::I_forward(1, 6));
+        instructions.push_back(new io::I_forward(2, 6));
+        instructions.push_back(new io::I_forward(3, 6));
+        instructions.push_back(new io::I_forward(4, 6));
+        instructions.push_back(new io::I_rotate(1, -2));
+        instructions.push_back(new io::I_rotate(2, -1));
+        instructions.push_back(new io::I_rotate(3, 1));
+        instructions.push_back(new io::I_rotate(4, 2));
+        io::print_instructions(instructions, cout, meta.current_flame);
+    }
     return 0;
 }
