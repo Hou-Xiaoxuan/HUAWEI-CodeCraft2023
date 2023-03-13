c8e6149ca8ed70a59dce6385a651e1574aa9b8d6
xv_rong <rongsheng_qv@outlook.com>
2023-03-14 00:24:24 +0800

    add spare

--- src/navigate.h
+++ src/navigate.h
@@ -22,14 +22,19 @@ double __get_max_robot_acceleration(const Robot &robot)
 /*速度调整*/
 void __change_speed(const Robot &robot, const Point &target, const vector<Point> &follow_target)
 {
+    if (robot.goods == 0)
+    {
+        instructions.push_back(new io::I_forward(robot.id, ConVar::max_robot_forward_speed));
+        return;
+    }
+    
     double min_a_x
         = __get_max_robot_acceleration(robot) * robot.v.x / (robot.v.x * robot.v.x + robot.v.y * robot.v.y);
     double min_a_y
         = __get_max_robot_acceleration(robot) * robot.v.y / (robot.v.x * robot.v.x + robot.v.y * robot.v.y);
-    double max_dis_x = robot.v.x * robot.v.x / (2 * min_a_x);
-    double max_dis_y = robot.v.y * robot.v.y / (2 * min_a_y);
+    double max_dis_x = robot.v.x * robot.v.x / (2 * min_a_x) + 0.3;
+    double max_dis_y = robot.v.y * robot.v.y / (2 * min_a_y) + 0.3;
     double next_v = 0;
-
     if (robot.loc.x + max_dis_x >= ConVar::map_weight || robot.loc.x - max_dis_x <= 0
         || robot.loc.y + max_dis_y >= ConVar::map_height || robot.loc.y - max_dis_y <= 0)
     {
@@ -52,7 +57,6 @@ void __change_direction(const Robot &robot, const Point &target, const vector<Po
     double x = target.x - robot.loc.x;
 
     double angle = atan2(y, x);
-    if (fabs(angle - robot.dirc) <= 1e-5) return;
 
     double delta = angle - robot.dirc;
     if (delta > M_PI)
@@ -81,8 +85,8 @@ void move_to(int robot_id, Point target, vector<Point> follow_target = vector<Po
     {
         instructions.push_back(new io::I_forward(robot_id, 0));
     }
-    __change_speed(robot, target, follow_target);
     __change_direction(robot, target, follow_target);
+    __change_speed(robot, target, follow_target);
 }
 }
 #endif
