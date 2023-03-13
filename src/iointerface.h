7458f1e03b27dced40bf949009f52a6a7de0ace1
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2023-03-13 14:39:14 +0800

    题目保证顺序，去掉id映射

--- src/iointerface.h
+++ src/iointerface.h
@@ -10,20 +10,8 @@
 
 namespace io
 {
-std::vector<int> robot_index_to_id;
-std::vector<int> robot_id_to_index;
-std::vector<int> station_index_to_id;    // index到输入顺序(id)
-std::vector<int> station_id_to_index;    // 输入id到index
-bool is_init = false;
 void read_flame(std::istream &io_in)
 {
-    if (is_init == false)
-    {
-        robot_id_to_index.resize(ConVar::max_robot + 1);
-        robot_index_to_id.resize(ConVar::max_robot + 1);
-        station_id_to_index.resize(ConVar::max_workstation + 1);
-        station_index_to_id.resize(ConVar::max_workstation + 1);
-    }
     // loc_to_index init
     int flame, money;
     io_in >> flame >> money;
@@ -36,36 +24,7 @@ void read_flame(std::istream &io_in)
     {
         Station tmp;
         io_in >> tmp.type >> tmp.loc.x >> tmp.loc.y >> tmp.timeleft >> tmp.material >> tmp.product;
-        if (is_init == false)
-        {
-            // 找到 station.loc = Pointer(x,y)的下标
-            int index = -1;
-            for (int j = 1; j < meta.station.size(); ++j)
-            {
-                if (Point::distance(meta.station[j].loc, tmp.loc) < 1e-3)
-                {
-                    index = j;
-                    break;
-                }
-            }
-            if (index == -1)
-            {
-                std::cerr << "[error][read_flame] station loc error, loc didn't find:(" << tmp.loc.x << ","
-                          << tmp.loc.y << ") with type " << tmp.type << std::endl;
-                // 打印所有station的loc
-                for (int j = 1; j < meta.station.size(); ++j)
-                {
-                    std::cerr << "[error][read_flame] staion_index " << j << ":(" << meta.station[j].loc.x
-                              << "," << meta.station[j].loc.y << ",type=" << meta.station[j].type
-                              << std::endl;
-                }
-                throw "station loc error";
-            }
-            station_id_to_index[i] = index;
-            station_index_to_id[index] = i;
-        }
-        int index = station_id_to_index[i];
-
+        int index = i+1;
         #ifdef DEBUG
                 if (meta.station[index].type != tmp.type){
                     std::cerr << "station type error" << std::endl;
@@ -79,29 +38,10 @@ void read_flame(std::istream &io_in)
         Robot tmp;
         io_in >> tmp.in_station >> tmp.goods >> tmp.time_factor >> tmp.crash_factor >> tmp.w >> tmp.v.x
             >> tmp.v.y >> tmp.dirc >> tmp.loc.x >> tmp.loc.y;
-        if (is_init == false)
-        {
-            int index = -1;
-            for (int j = 1; j < meta.robot.size(); j++)
-            {
-                if (Point::distance(meta.robot[j].loc, tmp.loc) < 1e-3)
-                {
-                    index = j;
-                    break;
-                }
-            }
-            if (index == -1)
-                throw "robot loc error";
-            robot_id_to_index[i] = index;
-            robot_index_to_id[index] = i;
-        }
 
-        int index = robot_id_to_index[i];
+        int index = i+1;
         meta.robot[index] = tmp;
     }
-    if (is_init == false)
-        is_init = true;
-
     std::string ok;
     io_in >> ok;
 }
@@ -116,7 +56,7 @@ struct I_forward : public Instruction {
     I_forward(int robot_id, double v) : Instruction(robot_id), v(v) { }
     void print(std::ostream &io_out) const override
     {
-        io_out << "forward " << robot_index_to_id[robot_id] << " " << v << std::endl;
+        io_out << "forward " << robot_id - 1 << " " << v << std::endl;
     }
 };
 struct I_rotate : public Instruction {
@@ -124,28 +64,28 @@ struct I_rotate : public Instruction {
     I_rotate(int robot_id, double w) : Instruction(robot_id), w(w) { }
     void print(std::ostream &io_out) const override
     {
-        io_out << "rotate " << robot_index_to_id[robot_id] << " " << w << std::endl;
+        io_out << "rotate " << robot_id - 1 << " " << w << std::endl;
     }
 };
 struct I_buy : public Instruction {
     I_buy(int robot_id) : Instruction(robot_id) { }
     void print(std::ostream &io_out) const override
     {
-        io_out << "buy " << robot_index_to_id[robot_id] << std::endl;
+        io_out << "buy " << robot_id - 1 << std::endl;
     }
 };
 struct I_sell : public Instruction {
     I_sell(int robot_id) : Instruction(robot_id) { }
     void print(std::ostream &io_out) const override
     {
-        io_out << "sell " << robot_index_to_id[robot_id] << std::endl;
+        io_out << "sell " << robot_id - 1 << std::endl;
     }
 };
 struct I_destroy : public Instruction {
     I_destroy(int robot_id) : Instruction(robot_id) { }
     void print(std::ostream &io_out) const override
     {
-        io_out << "destroy " << robot_index_to_id[robot_id] << std::endl;
+        io_out << "destroy " << robot_id - 1 << std::endl;
     }
 };
 
