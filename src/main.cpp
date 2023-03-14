6501ca82e6c45be5f5adcf06ebcae0ba113cc32e
xv_rong <rongsheng_qv@outlook.com>
2023-03-14 14:51:58 +0800

    redirect ceff

--- src/main.cpp
+++ src/main.cpp
@@ -20,22 +20,30 @@ int next_target(int st)
 
 int main()
 {
+    auto log = ofstream("log.txt");
+    cerr.rdbuf(log.rdbuf());
     init(cin);
     puts("OK");
     fflush(stdout);
 
     /*----------START----------*/
-    int robot_target = -1;             // aim
-    int robot_id = 1;                  // 使用1号机器人实验
+    int robot_target = -1;    // aim
+    int robot_id = 1;         // 使用1号机器人实验
     auto &robot = meta.robot[robot_id];
     robot_target = next_target(-1);    // 初始化目标点
     while (cin.eof() == false)
     {
-        cerr << "info: flame read" << endl;
+        // cerr << "info: flame read" << endl;
         io::read_flame(cin);
-        cerr << "info: flame read end, flame:" << meta.current_flame << endl;
+        // cerr << "info: flame read end, flame:" << meta.current_flame << endl;
         if (robot.in_station == robot_target)
         {
+            if (robot.goods == 1) navigate::instructions.push_back(new io::I_sell(robot_id));
+            if (meta.station[robot_target].product == 1)
+                navigate::instructions.push_back(new io::I_buy(robot_id));
+
+            navigate::instructions.push_back(new io::I_sell(robot_id));
+
             robot_target = next_target(robot_target);
         }
         auto target = meta.station[robot_target].loc;
