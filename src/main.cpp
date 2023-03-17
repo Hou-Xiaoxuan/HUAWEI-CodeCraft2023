8b93c85f09ebf55b6edf9ed28ca3c060ceee237e
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2023-03-17 17:14:49 +0800

    测试main

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,22 @@ using namespace std;
 #include "route_fool.h"
 int main()
 {
-    // auto fin = fstream("Robot/maps/1.txt");
-    // io::init(fin);
-    // route_fool::init();
-    // route_fool::give_pointing();
-    io::init(cin);
+    auto fin = fstream("Robot/maps/1.txt");
+    io::init(fin);
     route_fool::init();
-    puts("OK");
-    fflush(stdout);
-    /*----------START----------*/
-    while (cin.eof() == false)
-    {
-        cerr << "info: flame read" << endl;
-        io::read_flame(cin);
-        cerr << "info: flame read end, flame:" << meta.current_flame << endl;
-        route_fool::give_pointing();
-        io::print_instructions(io::instructions, cout, meta.current_flame);
-    }
+    route_fool::give_pointing();
+    // io::init(cin);
+    // route_fool::init();
+    // puts("OK");
+    // fflush(stdout);
+    // /*----------START----------*/
+    // while (cin.eof() == false)
+    // {
+    //     cerr << "info: flame read" << endl;
+    //     io::read_flame(cin);
+    //     cerr << "info: flame read end, flame:" << meta.current_flame << endl;
+    //     route_fool::give_pointing();
+    //     io::print_instructions(io::instructions, cout, meta.current_flame);
+    // }
     return 0;
 }
