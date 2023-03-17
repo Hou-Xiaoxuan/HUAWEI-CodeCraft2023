8a2f9df8383a782436cd01582db614e062058ae7
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2023-03-18 00:51:45 +0800

    清理逻辑，删去不用变量，增加空转惩罚

--- src/route_fool.h
+++ src/route_fool.h
@@ -86,7 +86,8 @@ int __estimated_move_flame(Point from, Point target_one, Point target_two = Poin
 void __give_pointing(int robot_id)
 {
     const auto &robot = meta.robot[robot_id];
-    int best_route_index = -1, best_profit_per_flame = 0;
+    int best_route_index = -1;
+    double best_profit_per_flame = 0;
     for (int i = 1; i < routes.size(); i++)
     {
         const auto &route = routes[i];
@@ -95,47 +96,33 @@ void __give_pointing(int robot_id)
             1-1.2. 没有其他机器人在处理该任务/同级别任务(运送相同货物到相同终点站)
             1-2. 目标点是8、9号点(仅收购，无需等待完成）「1-1、1-2满足一点即可」
             2. 预期到达from点时，预期money足够
+            3. 起点有货物(*优化可以加上可以预期得到货物*)
             4. 预期可以完成任务
             5. best_profit_per_flame最优
         *  expected_profit
-            -
-        如果target已经有部分原材料/已有其他原材料在运输，则计算时要在profit中加入target生产货物利益的一部分
-            -
-
+            如果target已经有部分原材料/已有其他原材料在运输，则计算时要在profit中加入target生产货物利益的一部分
+        *  expected_flame
+            如果需要空转等待，则在计算是添加一部分惩罚
         */
         const auto &target_station = meta.station[route.to_station_index];
         const auto &from_station = meta.station[route.from_station_index];
 
         int expected_material = target_station.material;    // 预期到达from点时已有的原材料
 
-        if (target_station.type != 8    // *contition 1-1.1
-            and target_station.type != 9 and target_station.goods_exist(route.goods))
-        {
-            // cerr << "[info][__pointing]"
-            //      << " [flame=]" << meta.current_flame << " roobot_id: " << robot_id << "with type "
-            //      << "skip route " << i << ": " << route << endl;
-            continue;
-        }
+        if (target_station.goods_exist(route.goods)) continue;    // *contition 1-1.1
+        if (from_station.with_product == 0) continue;            // *condition 3
 
         int expected_money = meta.current_money;       // 预期到达from点时的money
-        int expected_profit = route.profit;            // 预期利润
         int expected_buy_flame = meta.current_flame    // 预期到达from点时的flame
-            + __estimated_move_flame(robot.loc, meta.station[routes[i].from_station_index].loc);
+            + __estimated_move_flame(robot.loc, from_station.loc);
 
-        // [等待时间计算]: 预期到达from点时，预期生产好材料所需要的时间
-        int expected_ready_flame_count = ConVar::time_limit;    // from点预期生产好材料所需要的时间
-        if (from_station.with_product == 1)
-            expected_ready_flame_count = 0;
-        else if (from_station.timeleft > 0)
-            expected_ready_flame_count = min(expected_ready_flame_count, from_station.timeleft);
-        else
-            continue;    // 本论得不到货物则不再深入计算
 
-        int invalid_route = 0;
+        int invalid_route = 0;    // NOTE: invalid when [type not in (8&9) and 竞争终点] or [竞争起点]
         for (int j = 1; j < meta.robot.size(); j++)
         {
 
             if (processing[j] == 0) continue;
+            if (robot_id == j) continue;
 
             const auto &p_route = routes[processing[j]];
             if (p_route.to_station_index == route.to_station_index)
@@ -149,31 +136,34 @@ void __give_pointing(int robot_id)
                 }
             }
 
-            // TODO expected_money
-            if (p_route.finish_time <= expected_buy_flame)
-                expected_money += p_route.money_need + p_route.profit;
+            if (p_route.finish_time < expected_buy_flame and p_route.goods != 0)
+                expected_money += model::goods[p_route.goods].price - model::goods[p_route.goods].cost;
+            // 无法判断买货顺序实现先后，保险起见减去花费
+            if (p_route.finish_time > expected_buy_flame and p_route.goods == 0)
+                expected_money -= model::goods[route.goods].cost;
+
 
             // [等待时间计算] 竞争产物，退让
             if (p_route.from_station_index == route.from_station_index
                 and meta.robot[j].goods == 0)    // 起点相同
                 invalid_route = 100;
         }
-        // cerr << "[info][__pointing]"
-        //      << " [flame=]" << meta.current_flame << " roobot_id: " << robot_id
-        //      << "with invalid= " << invalid_route << "skip route " << i << ": " << route << endl;
+
         if (target_station.type == 8 || target_station.type == 9)
             invalid_route -= ConVar::max_robot;    // *condition 1-2
-
         if (invalid_route > 0) continue;
+
         if (expected_money < route.money_need) continue;    // *condition 2
 
+
+        /* 计算profit per flame(ppf)的参数，可调参数：
+           1.下一阶段收益的激励系数
+           2. 等待时间的惩罚系数 */
         // [预期利润计算]：增加下一阶段预期产物的利润
-        if (target_station.type == 8 or target_station.type == 9)
-        { }    // 啥也不做
-        else
+        int expected_profit = route.profit;    // 预期利润
+        if (target_station.type != 8 and target_station.type != 9)
         {
-            // expected_material的1的个数
-            int material_count = 0;
+            int material_count = 0;    // expected_material的1的个数
             while (expected_material)
             {
                 material_count += expected_material & 1;
@@ -183,31 +173,24 @@ void __give_pointing(int robot_id)
                 * material_count / target_station.product().needs.size();    // 加入预期利润0.5*原材料比例
         }
 
-        // 计算利润率
-        // int expected_flame_cost = max(expected_ready_flame_count,
-        //     __estimated_move_flame(robot.loc, meta.station[route.from_station_index].loc));
-        // expected_flame_cost += __estimated_move_flame(
-        //     meta.station[route.from_station_index].loc, meta.station[route.to_station_index].loc);
         int expected_flame_cost;
-        if (expected_buy_flame > __estimated_move_flame(robot.loc, from_station.loc))
+        if (route.finish_time > __estimated_move_flame(robot.loc, from_station.loc))
         {
             expected_flame_cost
-                = expected_buy_flame + __estimated_move_flame(from_station.loc, target_station.loc);
+                = route.finish_time + __estimated_move_flame(from_station.loc, target_station.loc);
         }
         else
         {
             expected_flame_cost = __estimated_move_flame(robot.loc, from_station.loc, target_station.loc);
         }
 
-        // cerr << "[info][__pointing]"
-        //      << " [flame=]" << meta.current_flame << " robot_id: " << robot_id
-        //      << " expected_flame_cost: " << expected_flame_cost << " expected_profit: " <<
-        //      expected_profit
-        //      << endl;
         if (meta.current_flame + expected_flame_cost > ConVar::time_limit) continue;    // *condition 4
-        if (expected_profit / expected_flame_cost > best_profit_per_flame)
+        double ppf = expected_profit * 1.0 / expected_flame_cost;
+        int empty_flame = max((route.finish_time - __estimated_move_flame(robot.loc, from_station.loc)), 0);
+        ppf -= empty_flame * 5;    // 空转惩罚，假设1000flame(20s)的预期收益是5000
+        if (ppf > best_profit_per_flame)
         {
-            best_profit_per_flame = expected_profit / expected_flame_cost;
+            best_profit_per_flame = ppf;
             best_route_index = i;
             cerr << "[info][__pointing] "
                  << " [flame=" << meta.current_flame << "] robot_id: " << robot_id
