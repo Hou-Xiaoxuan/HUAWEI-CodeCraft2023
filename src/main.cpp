ae3dc356b47cdf7b09f7affbb9a54fc005259607
LinXuan <linxuan1999@163.com>
2023-03-24 18:00:30 +0800

    增加map_recognize函数，解决clangd的傻逼bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,65 @@
-#include <fstream>
-#include <iostream>
-using namespace std;
 #include "anticollision.h"
 #include "iointerface.h"
 #include "model.h"
 #include "navigate.h"
 #include "route_fool.h"
+#include <fstream>
+#include <iostream>
+/*clangd的傻逼bug，main.cpp里的第一个函数不能被识别*/
+void sb_clangd() { }
 
 void robot()
 {
-    io::init(cin);
+    io::init(std::cin);
     route_fool::init();
     puts("OK");
     fflush(stdout);
     /*----------START----------*/
-    while (cin.eof() == false)
+    while (std::cin.eof() == false)
     {
         // cerr << "info: flame read" << endl;
-        io::read_flame(cin);
-        cerr << "info: flame read end, flame:" << meta.current_flame << endl;
+        io::read_flame(std::cin);
+        std::cerr << "info: flame read end, flame:" << meta.current_flame << std::endl;
         auto routes = route_fool::give_pointing();
         anticollision::anticollision(routes);
         for (auto in : io::instructions)
         {
-            in->print(cerr);
+            in->print(std::cerr);
         }
-        io::print_instructions(io::instructions, cout, meta.current_flame);
+        io::print_instructions(io::instructions, std::cout, meta.current_flame);
     }
+}
+
+
+/*硬编码，识别是几号地图*/
+int map_recognize()
+{
+    if (Point::distance(meta.station[1].loc, {24.75, 49.25}) < 1e-5) return 1;
+    if (Point::distance(meta.station[1].loc, {0.75, 49.25}) < 1e-5) return 2;
+    if (Point::distance(meta.station[1].loc, {23.25, 49.25}) < 1e-5) return 3;
+    if (Point::distance(meta.station[1].loc, {24.75, 46.25}) < 1e-5) return 4;
+
+    return 0;
 }
 void local()
 {
-    auto fin = fstream("Robot/maps/1.txt");
+
+    auto fin = std::fstream("Robot/maps/3.txt");
     io::init(fin);
+    std::cerr << "[start] map recognize: " << map_recognize() << std::endl;
     route_fool::init();
     route_fool::give_pointing();
 }
 int main()
 {
     // cerror重定向到文件
-    fstream fout("../log.txt", ios::out);
+    std::fstream fout("../log.txt", std::ios::out);
     if (fout.is_open())
-        cerr.rdbuf(fout.rdbuf());
+        std::cerr.rdbuf(fout.rdbuf());
     else
-        cerr << "[error] log file open failed" << endl;
+        std::cerr << "[error] log file open failed" << std::endl;
     // local();
     robot();
+
     return 0;
 }
