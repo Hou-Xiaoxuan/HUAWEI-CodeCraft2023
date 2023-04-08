9e3d0cd8455d68e54b83ecd3131499c8b1e0b821
xv_rong <rongsheng_qv@outlook.com>
2023-04-09 01:12:26 +0800

    修复点在stopline内 无法行走

--- src/find_path_squre.h
+++ src/find_path_squre.h
@@ -14,7 +14,6 @@ namespace find_path_square
 using namespace std;
 using namespace navmesh;
 
-
 struct Pos {
     int index_x = -1;
     int index_y = -1;
@@ -26,6 +25,13 @@ int pre_cnt_path = 0;
 
 int cnt_find_que = 0;
 int pre_cnt_find_que = 0;
+// vector<vector<Vertex>> proper_pos;
+vector<vector<Pos>> pre = vector<vector<Pos>>(Map::width + 2, vector<Pos>(Map::height + 2));
+
+Vertex start;
+Vertex target;
+bool have_good;
+
 vector<pair<int, int>> dirs = {
     { 0,  1},
     { 1,  0},
@@ -33,40 +39,22 @@ vector<pair<int, int>> dirs = {
     {-1,  0},
 };
 
-// 检查line是否跟多边形相交
-bool _check_cross(Segment line)
+bool _is_valid(Segment line)
 {
-    for (auto &poly : trans_map::polys)
-        for (int i = 1; i < poly.points.size(); i++)
-            if (Segment::is_cross(line, {poly.points[i - 1], poly.points[i]})) return true;
-
-    return false;
-}
-bool _check_valid(Segment line)
-{
-    if (std::hypot(line.a.x - line.b.x, line.a.y - line.b.y) < EPS) return true;
-    auto start = line.a;
-    auto target = line.b;
-    bool valid = true;
-    if (_check_cross({start, target})) valid = false;
-    if (valid)
+    ++cnt_path;
+    double limit_dis = have_good ? ConVar::robot_radius_goods * 2 : ConVar::robot_radius * 2;
+    for (const auto &poly : trans_map::polys)
     {
-        // 检测线段沿着垂直方向移动后是否可以直接连线
-        Vec2 rotate_dir = Vec2 {start, target}.rotate(M_PI / 2) * 10;
-        rotate_dir = rotate_dir / rotate_dir.length() * 0.6;
-        Vertex start_1 = {start.x + rotate_dir.x, start.y + rotate_dir.y};
-        Vertex target_1 = {target.x + rotate_dir.x, target.y + rotate_dir.y};
-        valid = !_check_cross({start_1, target_1});
+        const auto &points = poly.points;
+        for (int j = 0; j < points.size(); ++j)
+        {
+            if (Segment::distance(line, {points[j], points[(j + 1) % points.size()]}) < limit_dis)
+            {
+                return false;
+            }
+        }
     }
-    if (valid)
-    {
-        Vec2 rotate_dir = Vec2 {start, target}.rotate(-M_PI / 2) * 10;
-        rotate_dir = rotate_dir / rotate_dir.length() * 0.6;
-        Vertex start_1 = {start.x + rotate_dir.x, start.y + rotate_dir.y};
-        Vertex target_1 = {target.x + rotate_dir.x, target.y + rotate_dir.y};
-        valid = !_check_cross({start_1, target_1});
-    }
-    return valid;
+    return true;
 }
 
 // 找到当前点所在的meta.map中的正方形 返回值 正方形x轴index y轴index 这个点坐标
@@ -101,76 +89,6 @@ auto cmp_pos = [](const pair<Pos, int> &a, const pair<Pos, int> &b) -> bool {
                 < trans_map::nearest_obstacle[b.first.index_x][b.first.index_y]);
 };
 
-
-// vector<vector<Vertex>> proper_pos;
-vector<vector<Pos>> pre;
-
-Vertex start;
-Vertex target;
-bool have_good;
-
-// void get_proper_pos()
-// {
-//     double limit_dis = ConVar::robot_radius_goods + 0.05;
-
-//     for (int i = 1; i <= Map::width; ++i)
-//     {
-//         for (int j = 1; j <= Map::height; ++j)
-//         {
-//             if (meta.map[i][j] == '#') continue;
-
-//             Vertex center = proper_pos[i][j];
-//             Vertex nearest_vertex;
-//             double nearest_dis = 1e9;
-//             for (const auto &poly : trans_map::polys)
-//             {
-//                 const auto &points = poly.points;
-//                 for (int k = 0; k < points.size(); ++k)
-//                 {
-//                     Segment line = {points[k], points[(k + 1) % points.size()]};
-//                     double tmp_dis = dis_point_to_segment(center, line);
-//                     if (tmp_dis < nearest_dis)
-//                     {
-//                         nearest_dis = tmp_dis;
-//                         nearest_vertex = point_point_to_segment(center, line);
-//                     }
-//                 }
-//             }
-
-//             if (nearest_dis < limit_dis)
-//             {
-//                 Vec2 v = {nearest_vertex, center};
-//                 // c 沿着 v 方向移动 limit_path
-//                 // XXX v 理论上应该不可能为 0
-//                 Vec2 v_unit = {v.x / v.length(), v.y / v.length()};
-//                 Vertex new_c
-//                     = {nearest_vertex.x + v_unit.x * limit_dis, nearest_vertex.y + v_unit.y * limit_dis};
-//                 fstream fout("proper.txt", ios::app);
-//                 fout << nearest_vertex.x << " " << nearest_vertex.y << " " << proper_pos[i][j].x << " "
-//                      << proper_pos[i][j].y << " " << new_c.x << " " << new_c.y << endl;
-//                 proper_pos[i][j] = new_c;
-//             }
-//         }
-//     }
-// }
-
-void init()
-{
-    // proper_pos = vector<vector<Vertex>>(Map::width + 2, vector<Vertex>(Map::height + 2));
-    pre = vector<vector<Pos>>(Map::width + 2, vector<Pos>(Map::height + 2));
-
-    // for (int i = 1; i <= Map::width; ++i)
-    // {
-    //     for (int j = 1; j <= Map::height; ++j)
-    //     {
-    //         proper_pos[i][j] = get_center(i, j);
-    //     }
-    // }
-    // for (int i = 0; i < 1; ++i)
-    // {
-    //     // get_proper_pos();
-    // }
-}
 
 vector<Vertex> get_ori_path()
 {
@@ -215,12 +133,17 @@ vector<Vertex> get_ori_path()
 
             bool is_stop = false;
             Vertex ncenter = get_center(nx, ny);
-            for (const auto &line : trans_map::stop_line)
+            if (start_pos.index_x != now_pos.first.index_x or start_pos.index_y != now_pos.first.index_y)
             {
-                if (Segment::is_cross(line, Segment {now_center, ncenter}))
+                for (const auto &line : trans_map::stop_line)
                 {
-                    is_stop = true;
-                    break;
+                    {
+                        if (Segment::is_cross(line, Segment {now_center, ncenter}))
+                        {
+                            is_stop = true;
+                            break;
+                        }
+                    }
                 }
             }
             if (is_stop and not(nx == target_pos.index_x and ny == target_pos.index_y)) continue;
@@ -296,22 +219,10 @@ vector<Vertex> get_smooth_path(const vector<Vertex> &ori_path)
         int end_index = 0;
         for (int i = start_index + 1; i < ori_path.size(); ++i)
         {
-            bool is_valid = true;
             Segment tmp = {ori_path[start_index], ori_path[i]};
             // TODO 复杂度高
-            for (const auto &poly : trans_map::polys)
-            {
-                const auto &points = poly.points;
-                for (int j = 0; j < points.size(); ++j)
-                {
-                    if (Segment::distance(tmp, {points[j], points[(j + 1) % points.size()]}) < limit_dis)
-                    {
-                        is_valid = false;
-                        break;
-                    }
-                }
-                if (not is_valid) break;
-            }
+
+            bool is_valid = _is_valid(tmp);
 
             if (i == ori_path.size() - 1)
             {
@@ -417,7 +328,7 @@ vector<Vertex> find_path(const Vertex &_start, const Vertex &_target, bool _have
     have_good = _have_good;
     const auto &ori_path = get_ori_path();
     const auto &smooth_path = get_smooth_path(ori_path);
-    return smooth_path_again(smooth_path);
+    return smooth_path;
 }
 
 vector<Vertex>
@@ -433,7 +344,6 @@ find_shelter_path(const vector<Vertex> &sub_path, const vector<vector<Vertex>> &
     bool is_in_path = false;
     for (const auto &tmp_path : pri_path)
     {
-
         for (int i = 0; i < tmp_path.size() and not is_in_path; ++i)
         {
             if (dis_point_to_segment(start, {tmp_path[i], tmp_path[(i + 1) % tmp_path.size()]})
@@ -525,12 +435,15 @@ find_shelter_path(const vector<Vertex> &sub_path, const vector<vector<Vertex>> &
 
             bool is_stop = false;
             Vertex ncenter = get_center(nx, ny);
-            for (const auto &line : trans_map::stop_line)
+            if (start_pos.index_x != now_pos.first.index_x or start_pos.index_y != now_pos.first.index_y)
             {
-                if (Segment::is_cross(line, Segment {now_center, ncenter}))
+                for (const auto &line : trans_map::stop_line)
                 {
-                    is_stop = true;
-                    break;
+                    if (Segment::is_cross(line, Segment {now_center, ncenter}))
+                    {
+                        is_stop = true;
+                        break;
+                    }
                 }
             }
 
@@ -597,147 +510,6 @@ find_shelter_path(const vector<Vertex> &sub_path, const vector<vector<Vertex>> &
     }
 }
 
-
-// vector<Vertex> find_shelter(const Vertex &start, const vector<Vertex> &dodge_path, bool have_good)
-// {
-//     // 远离寻找dodge_path起始3m以内的一个躲避点
-//     // 如果没有 就先前往dodge_path的终点
-//     double limit_dis = have_good ? ConVar::robot_radius_goods * 2 : ConVar::robot_radius * 2;
-//     limit_dis *= 1.1;
-//     // 如果当前位置不挡道(完整路径)：留在原地
-//     bool can_stop = true;
-//     for (int i = 1; i < dodge_path.size() and can_stop == true; ++i)
-//     {
-//         if (dis_point_to_segment(start, {dodge_path[i], dodge_path[i - 1]}) < limit_dis) can_stop =
-//         false;
-//     }
-//     if (can_stop) return vector<Vertex> {start, start};
-
-//     vector<Vertex> dodge_path_sub;
-//     double tmp_dis = 0;
-//     for (int i = 1; i < dodge_path.size(); ++i)
-//     {
-//         tmp_dis += std::hypot(
-//             dodge_path.at(i).x - dodge_path.at(i - 1).x, dodge_path.at(i).y - dodge_path.at(i - 1).y);
-//         if (tmp_dis > 5)
-//         {
-//             dodge_path_sub = vector<Vertex>(dodge_path.begin(), dodge_path.begin() + i);
-//             break;
-//         }
-//     }
-//     if (dodge_path_sub.empty()) dodge_path_sub = dodge_path;
-
-//     auto &dodge_loc = dodge_path[0];
-
-//     Vertex target {-1, -1};
-//     /* BFS寻找躲避点*/
-//     {
-//         // cerr << "[debug][find_shelter] enter bfs" << endl;
-//         // 1. 禁止靠近dodge_loc
-//         // 2. 距离dodge_sub_path距离大于limit_dis
-//         // 3. 15步数内能到达
-//         priority_queue<pair<Pos, int>, vector<pair<Pos, int>>, decltype(cmp_pos)> que(cmp_pos);
-//         queue<Pos> que_bak;
-//         auto start_pos = current_pos(start);
-//         pre[start_pos.index_x][start_pos.index_y].index_x = -1;
-//         que.push({start_pos, 0});
-//         que_bak.push(start_pos);
-
-//         pair<Pos, int> now_pos {};
-//         while (not que.empty())
-//         {
-//             now_pos = que.top();
-//             // cerr << "[debug][find_shelter][bfs] now_pos: " << now_pos.first.index_x << " "
-//             //  << now_pos.first.index_y << " " << now_pos.second << endl;
-//             que.pop();
-//             // check valid
-//             if (now_pos.second > 20) break;
-//             bool valid = true;
-//             for (int i = 1; i < dodge_path_sub.size() and valid; i++)
-//             {
-//                 if (dis_point_to_segment(
-//                         now_pos.first.pos, {dodge_path_sub.at(i), dodge_path_sub.at(i - 1)})
-//                     <= limit_dis)
-//                     valid = false;
-//             }
-//             if (valid)    // 目标点要求
-//             {
-//                 target = now_pos.first.pos;
-//                 break;
-//             }
-
-//             /*next*/
-//             Vertex now_center = get_center(now_pos.first.index_x, now_pos.first.index_y);
-//             for (int i = 0; i < 4; ++i)
-//             {
-//                 // cerr << "[debug][find_shelter][bfs] i=" << i << ", and pos is " <<
-//                 now_pos.first.index_x
-//                 //    << " " << now_pos.first.index_y << endl;
-//                 int nx = now_pos.first.index_x + dirs.at(i).first;
-//                 int ny = now_pos.first.index_y + dirs.at(i).second;
-
-//                 if (meta.map.at(nx).at(ny) == '#') continue;
-//                 if (std::hypot(nx - dodge_loc.x, ny - dodge_loc.y) < limit_dis) continue;
-
-//                 auto &npre = pre.at(nx).at(ny);
-//                 if (npre.index_x != -1) continue;
-
-//                 bool is_stop = false;
-//                 Vertex ncenter = get_center(nx, ny);
-//                 for (const auto &line : trans_map::stop_line)
-//                 {
-//                     if (Segment::is_cross_2(line, Segment {now_center, ncenter}))
-//                     {
-//                         is_stop = true;
-//                         break;
-//                     }
-//                 }
-//                 if (is_stop) continue;
-
-//                 for (const auto &line : trans_map::danger_line)
-//                 {
-//                     if (not Segment::is_cross_2(line, Segment {now_center, ncenter})) continue;
-
-//                     if (line.length() <= limit_dis)
-//                     {
-//                         is_stop = true;
-//                         break;
-//                     }
-//                 }
-//                 if (is_stop) continue;
-
-//                 // 偏移起点和终点之外所有点
-//                 Pos npos = {nx, ny, proper_pos[nx][ny]};
-
-//                 npre = now_pos.first;
-//                 // cerr << "[debug][find_shelter][bfs] next_pos: " << npos.index_x << " " << npos.index_y
-//                 //      << " " << now_pos.second + 1 << endl;
-//                 que.push({npos, now_pos.second + 1});
-//                 que_bak.push(npos);
-//             }
-//         }
-
-//         // 释放内存
-//         while (not que_bak.empty())
-//         {
-//             Pos now_pos = que_bak.front();
-//             que_bak.pop();
-//             pre[now_pos.index_x][now_pos.index_y].index_x = -1;
-//         }
-//         /*BFS END*/
-//     }
-
-//     if (target.x != -1)
-//     {
-//         cerr << "[info][find_shelter] find shelter target" << target << endl;
-//         return find_path(start, target, have_good);
-//     }
-
-
-//     else    // XXX 找不到躲避点
-//         return {start, start};
-// }
-
 // 找到最近的工作站,limit步数内没有找到则返回空
 std::vector<Vertex> find_nearest_workshop(const Vertex &start)
 {
@@ -784,12 +556,16 @@ std::vector<Vertex> find_nearest_workshop(const Vertex &start)
 
                 bool is_stop = false;
                 Vertex ncenter = get_center(nx, ny);
-                for (const auto &line : trans_map::stop_line)
+                if (start_pos.index_x != now_pos.first.index_x
+                    or start_pos.index_y != now_pos.first.index_y)
                 {
-                    if (Segment::is_cross_2(line, Segment {now_center, ncenter}))
+                    for (const auto &line : trans_map::stop_line)
                     {
-                        is_stop = true;
-                        break;
+                        if (Segment::is_cross(line, Segment {now_center, ncenter}))
+                        {
+                            is_stop = true;
+                            break;
+                        }
                     }
                 }
                 if (is_stop) continue;
